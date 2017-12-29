ddf73991d4d0e58c38f1101456f80489e48a39ca
donoghuc <cas.donoghue@gmail.com>
2017-12-29 12:07:26 -0800

    continue adding in critical building blocks

---
+++ casadilla_app/controllers/base_controller.py
@@ -0,0 +1,34 @@
+import casadilla_app.infrastructure.static_cache as static_cache
+import pyramid.renderers
+import pyramid.httpexceptions as exc
+
+from casadilla_app.infrastructure.supressor import suppress
+
+
+class BaseController:
+    def __init__(self, request):
+        self.request = request
+        self.build_cache_id = static_cache.build_cache_id
+
+        layout_render = pyramid.renderers.get_renderer('casadilla_app:templates/shared/_layout.pt')
+        impl = layout_render.implementation()
+        self.layout = impl.macros['layout']
+
+
+    # noinspection PyMethodMayBeStatic
+    @suppress()
+    def redirect(self, to_url, permanent=False):
+        if permanent:
+            raise exc.HTTPMovedPermanently(to_url)
+        raise exc.HTTPFound(to_url)
+
+
+    @property
+    def data_dict(self):
+        data = dict()
+        data.update(self.request.GET)
+        data.update(self.request.POST)
+        data.update(self.request.matchdict)
+
+        return data
+
